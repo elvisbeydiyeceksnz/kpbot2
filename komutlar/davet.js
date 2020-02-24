9bb1692c3941b3f318cf6351b0488e0ab2f18f72
elvisbeydiyeceksnz <57803893+elvisbeydiyeceksnz@users.noreply.github.com>
2020-02-25 01:03:50 +0300

    Add files via upload

---
+++ komutlar/davet.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+
+let botid = ('650739604789395476') //bu yere botun id'sini yapıştırın.
+//eğer botunuz dbl(discord bot list) de yoksa Bota Oy Ver (Vote) olmucaktır.
+
+exports.run = (client, message, args) => {
+    const embed = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setAuthor(`${client.user.username} İletişim Bilgileri`)
+    .addField('BİZ KİMİZ?', 'KaptainPhenix Ailesiyiz :)')//ne kadar yetkili komutunuz varsa o kadar .addField('prefix+komut', 'açıklama/kullanım amacı') koyun
+    .addField(`» Linkler`, `[Bot Sunucuya Özel Kodlanmıştır] **|** [Destek Sunucusu](https://discord.gg/4tXRufy) **|** [Web Sitesi ( Yakında )]()`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
+    message.channel.sendEmbed(embed);
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0,
+};
+
+exports.help = {
+  name: 'bizkimiz',
+  description: 'bot hakkında bilgi',
+  usage: 'davetet'
+};
+   
